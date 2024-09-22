a64a647606c9d1e3e5c66f01fa46a39d9cc9dc93
MeesHvnrs <78762910+MeesHvnrs@users.noreply.github.com>
2024-09-22 23:08:39 +0200

    more frontend changes.

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,11 +1,19 @@
-﻿namespace Blok1.Models
+﻿using System.ComponentModel.DataAnnotations; 
+namespace Blok1.Models
 {
     public class Product
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string Comment { get; set; } = null!;
+
+        [Required]
+        public string Name { get; set; } = null!; 
+
+        public string? Comment { get; set; }
+        [Required]
         public double Price { get; set; }
+
         public bool ColorChange { get; set; }
     }
 }
+
+
